85e7b4ac7b6c78b17ac948025b5a3817176f4059
KunbaoG8168 <3B017096@gm.student.ncut.edu.tw>
2026-02-03 16:17:34 +0800

    upload file test

--- models/case.py
+++ models/case.py
@@ -1,5 +1,5 @@
 #uuid 是 Python 內建模組
-#用來產生「全世界幾乎不會重複的唯一識別碼」。
+#用來產生唯一識別碼
 import uuid 
 
 
@@ -10,24 +10,6 @@ class Case:
         self.application_number = None #法律識別
         self.filing_date = None #申請日
         self.documents = [] #一個Case會有很多文件，用list
-        self.events = [] 
+        self.events = [] #一個Case會有很多event，用list
         self.deadlines = [] #一個event會有多個狀態, 且Deadline有自己的狀態
         self.tasks = [] #法律義務跟工作職務分離
-
-        
-
-
-'''
-class Case:
-    def __init__(self):
-        self.case_id = str(uuid.uuid4())  # 系統自己產
-        #3f2c1d8a-9b6f-4c42-8c3e-2b8d9e7f91a1
-        self.jurisdiction = None
-        self.application_number = None
-        self.filing_date = None
-
-        self.documents = []
-        self.events = []
-        self.deadlines = []
-        self.tasks = []
-'''
